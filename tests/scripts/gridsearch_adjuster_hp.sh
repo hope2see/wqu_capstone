566001a89e040d78f180dcc93d61b6cac8d741d1
hope2see <ungyo.jung@gmail.com>
2025-03-06 03:22:34 +0900

    fix typo

--- tests/scripts/gridsearch_adjuster_hp.sh
+++ tests/scripts/gridsearch_adjuster_hp.sh
@@ -6,16 +6,16 @@ desc='chosen_models_BTC'
 
 
 for gpm_lookback_win in 10 30 50 -1; do 
-for lookback_window in 3 5 10; do 
+for lookback_win in 3 5 10; do 
 for discount_factor in 1.0 1.3 1.5 2.0; do 
 for avg_method in 0 1; do 
 for weighting_method in 0 1; do
 python -u run.py \
     --model TABE \
-    --model_id $test_name'_('$desc')_'$gpm_lookback_win'_'$lookback_window'_'$discount_factor'_'$avg_method'_'$weighting_method'_'$scaling_factor \
+    --model_id $test_name'_('$desc')_'$gpm_lookback_win'_'$lookback_win'_'$discount_factor'_'$avg_method'_'$weighting_method'_'$scaling_factor \
     --data TABE_FILE --data_path 'BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv' \
     --train_epochs 3  \
-    --adjuster '--gpm_lookback_win '$gpm_lookback_win' --lookback_window  '$lookback_window' '$discount_factor' --avg_method '$avg_method' --weighting_method '$weighting_method \
+    --adjuster '--gpm_lookback_win '$gpm_lookback_win' --lookback_win  '$lookback_win' '$discount_factor' --avg_method '$avg_method' --weighting_method '$weighting_method \
     --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' \
     --basemodel 'EtsModel' \
     --basemodel 'Drifter' \
@@ -28,17 +28,17 @@ done
 
 
 for gpm_lookback_win in 10 30 50 -1; do 
-for lookback_window in 3 5 10; do 
+for lookback_win in 3 5 10; do 
 for discount_factor in 1.0 1.3 1.5 2.0; do 
 for avg_method in 0 1; do 
 for weighting_method in 2; do
 for scaling_factor in 1 10 30 100; do 
 python -u run.py \
     --model TABE \
-    --model_id $test_name'_('$desc')_'$gpm_lookback_win'_'$lookback_window'_'$discount_factor'_'$avg_method'_'$weighting_method'_'$scaling_factor \
+    --model_id $test_name'_('$desc')_'$gpm_lookback_win'_'$lookback_win'_'$discount_factor'_'$avg_method'_'$weighting_method'_'$scaling_factor \
     --data TABE_FILE --data_path 'BTC-USD_LogRet_2021-01-01_2023-01-01_1d.csv' \
     --train_epochs 3  \
-    --adjuster '--gpm_lookback_win '$gpm_lookback_win' --lookback_window  '$lookback_window' '$discount_factor' --avg_method '$avg_method' --weighting_method '$weighting_method' --scaling_factor '$scaling_factor \
+    --adjuster '--gpm_lookback_win '$gpm_lookback_win' --lookback_win  '$lookback_win' '$discount_factor' --avg_method '$avg_method' --weighting_method '$weighting_method' --scaling_factor '$scaling_factor \
     --combiner '--lookback_win 1 --discount_factor 3.0 --avg_method 0 --weighting_method 1 --max_models 2' \
     --basemodel 'EtsModel' \
     --basemodel 'Drifter' \
